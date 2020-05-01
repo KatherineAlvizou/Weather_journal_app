ad0588f2d4b7801cbcd63e41008a91ed4a754321
Katerina Alvizou <katherine.alvizou@gmail.com>
2020-05-01 11:51:36 +0200

    server set up, POST, GET routes added and app functionality

--- server.js
+++ server.js
@@ -1,29 +1,49 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
+let projectData={};
 // Require Express to run server and routes
 const express= require('express');
+//body-parser package will be used as middleware
+const bodyParser= require('body-parser');
 // Start up an instance of app
 const app= express();
 
-//including cors and connecting it to express app instanceconst 
-const cors= require('cors');
-app.use(cors());
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors= require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
 const port= 8000;
-const server= app.listen(port, listening);
+const server = app.listen(port, listening);
 
 function listening(){
-    console.log(`running on localhost: ${port}`);
+    console.log(`server running on localhost: ${port}`);
 }
+
+//Adding a GET route that returns the projectData object 
+app.get('/all', function(request,response){
+    response.send(projectData);
+});
+
+//POST route to store the data
+
+app.post('/add', addData);
+
+ function addData(request,response){
+     
+     let data=request.body;
+     projectData['temp']=data.temp;
+    projectData['date']=data.date;
+    projectData['feelings']=data.feelings;
+    response.send(projectData);
+    console.log(projectData);
+};
+
--- website/app.js
+++ website/app.js
@@ -1,5 +1,95 @@
+
+const apiKey= '&APPID=0f50edcc95b1786a410ce311fe0e311c';
+let baseURL='http://api.openweathermap.org/data/2.5/weather?q=';
+const metric= '&units=metric';
 /* Global Variables */
 
+const generate= document.getElementById('generate');
+const content= document.getElementById('content');
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+//functions
+
+generate.addEventListener('click', performAction);
+function performAction(e){
+  addData()
+  .then(updateUI());  
+};
+
+
+//GET web API 
+const getApi= async()=>{
+    const zip= document.getElementById('zip').value;
+  const weatherApi= await fetch(baseURL+zip+apiKey+metric);
+  console.log(baseURL+zip+apiKey);
+  try{
+     const weather= await weatherApi.json();
+     const temp=weather.main.temp;
+     return temp;
+ }catch(error){
+     console.log('error', error);
+ }
+}
+
+const addData= async()=>{
+  const weather= await getApi();
+  const feelings= document.getElementById('feelings').value;
+  postData('/add', {temp:weather, date: newDate, feelings:feelings});
+
+}
+
+
+ //update the UI
+const updateUI= async()=>{
+  const request= await fetch('/all');
+  try{
+    const allData= await request.json();
+    document.getElementById('date').innerHTML = `Date: ${allData.date}`;
+    document.getElementById('temp').innerHTML = `Temperature is : ${allData.temp}&#8451;`;
+    document.getElementById('content').innerHTML = `You feel: ${allData.feelings}`;
+  }catch(error){
+      console.log('error', error);
+  }
+  }; 
+
+//POST data
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+             
+      body: JSON.stringify(data), 
+    });
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
+
+  
+
+// GET project data
+
+  const getData = async(url='')=>{
+    const response = await fetch(url)
+    try { 
+      const data = await response.json();
+      console.log(data)
+      return data;
+    }  catch(error) {
+      console.log("there is an error", error);
+      
+    }
+  };
+  
+
